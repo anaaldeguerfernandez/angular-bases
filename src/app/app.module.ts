4b8c4e7083c9d504ab49c57bbeae9137b39f8045
Ana Aldeguer <anaaldfer@gmail.com>
2024-08-15 11:39:18 +0200

    Primer commit del proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 // Después librerías de terceros.
 import { CounterModule } from './counter/counter.module';
+import { DbzModule } from './dbz/dbz.module';
 import { HeroesModule } from './heroes/heroes.module'
 
 // Por último nuestro código.
@@ -17,6 +18,7 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     CounterModule,
+    DbzModule,
     HeroesModule,
   ],
   providers: [],
